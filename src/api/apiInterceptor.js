780d3c991492d241fb4b9a50116edb843c14b6a5
Sohail Shaikh <sohailshaikh062003@gmail.com>
2025-12-06 21:18:39 +0530

    fix: Update API base URL to use environment variable

--- src/api/apiInterceptor.js
+++ src/api/apiInterceptor.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:4200/api/";
+// const BASE_URL = "http://localhost:4200/api/";   
+const BASE_URL = import.meta.env.VITE_BASE_URL; 
 
 export const apiInterceptor = async (url, options = {}) => {
   const token = localStorage.getItem("accessToken");
